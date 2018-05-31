71deb662a5e78964928d911136dbcfb366f4b446
Dzimid <jac3k.dziemidowicz@gmail.com>
2018-05-31 13:35:40 +0200

    Utworzenie formularza do edycji firmy

---
+++ src/AppBundle/Form/CompanyEditForm.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CompanyEditForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, array('required' => true, 'label' => 'Company Name:'))
+            ->add('description', TextareaType::class, array('required' => true, 'label' => 'Company Description:'))
+            ->add('save', SubmitType::class, array('label' => 'Save', 'attr' => ['class' => 'btn btn-primary']));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_bundle_company_edit_form';
+    }
+}
