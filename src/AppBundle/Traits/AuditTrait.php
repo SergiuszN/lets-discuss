312e0f3120cc2802a87f5abf1c24480b26eec66f
SergiuszN <serg.ni2011@gmail.com>
2018-06-17 12:13:05 +0200

    Database security

---
+++ src/AppBundle/Traits/AuditTrait.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Traits;
+
+use AppBundle\Entity\Audit;
+use Doctrine\Bundle\DoctrineBundle\Registry;
+
+trait AuditTrait
+{
+    /**
+     * @param array $data
+     * @return void
+     */
+    public function saveAudit($data)
+    {
+        /** @var Registry $doctrine */
+        $doctrine = $this->getDoctrine();
+        $em = $doctrine->getManager();
+
+        $audit = new Audit();
+        $audit->setDate(new \DateTime());
+        $audit->setUser($this->getUser());
+        $audit->setAction($this->getRequest()->attributes->get('_controller'));
+        $audit->setIp($this->get('request_stack')->getCurrentRequest()->getClientIp());
+        $audit->setParameters(json_encode($data));
+
+        $em->persist($audit);
+        $em->flush();
+    }
+}
